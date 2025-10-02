9ed6a81c8f0aaec5a7adbac94fe0665508e92ef0
zanele maseko <itszanele@gmail.com>
2025-10-02 19:24:48 +0200

    fetch-api

---
+++ fetch-data.js
@@ -0,0 +1,33 @@
+async function  fetchUserData() {
+
+    const apiUrl= 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer= document.getElementById('api-data');
+
+    try {
+        const response= await fetch(apiUrl);
+        const users= await response.json();
+
+        dataContainer.innerHTML= '';
+
+        const userList= document.createElement('ul');
+
+        users.forEach(element => {
+
+            const userList= document.createElemen('li');
+            li.textContent=user.name;
+            userList.appendChild(li);
+
+            
+        });
+        
+
+        dataContainer.appendChild(userList);
+    } catch (error) {
+        dataContainer.innerHTML= 'Failed to load user data';
+
+        console.log(error);
+    }
+}
+
+ document.addEventListener('DOMContentLoaded', fetchUserData);
